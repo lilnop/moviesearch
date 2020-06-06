451850d6d2c98ecd9e7c23bd925767b0eae0731b
lilnop <lilsnop@gmail.com>
2020-06-05 20:23:30 -0700

    Updates

---
+++ precache-manifest.bf2b448de559b3d1b32b3a07831517e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d00bcbc9f46c68cea4a749b03b23a8f",
+    "url": "/moviesearch/index.html"
+  },
+  {
+    "revision": "8af8c4b4adca42613e61",
+    "url": "/moviesearch/static/css/main.81f65859.chunk.css"
+  },
+  {
+    "revision": "1f9cf1a458a591544373",
+    "url": "/moviesearch/static/js/2.a6426f54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearch/static/js/2.a6426f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af8c4b4adca42613e61",
+    "url": "/moviesearch/static/js/main.a688b42f.chunk.js"
+  },
+  {
+    "revision": "72bc980e5423bb951719",
+    "url": "/moviesearch/static/js/runtime-main.d7287340.js"
+  }
+]);
